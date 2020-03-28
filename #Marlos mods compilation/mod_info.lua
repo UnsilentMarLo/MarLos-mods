b6a1029b5aff39aff56f5f5aabc68449a9f919bb
Marlo Kruse <Plays.xenon@yahoo.de>
2020-03-28 21:15:13 +0100

    Update NO84

--- #Marlos mods compilation/mod_info.lua
+++ #Marlos mods compilation/mod_info.lua
@@ -1,11 +1,11 @@
 name = "Marlo's mod Compilation"
-version = 81
+version = 82
 copyright = "Copyright © 2018, Electronic Farts co"
 description = "This mod contains multiple new units (other factions are WIP) Disclaimer: These Units were made/edited by me MarLo aka Degulum, based on vanilla units with varying quality and uniquemess"
 author = "Degulum"
 icon = "/mods/#Marlos mods compilation/icon.bmp"
 url = ""
-uid = "e32uef2r12-d1321-1Rdf-14r8-2467d3c1s38cd"
+uid = "e32uef1312-d1321-1Rdf-14r8-2467d3c1s38cd"
 
 exclusive = false
 
