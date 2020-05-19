4f38ee5a1abfa7feb35634a3b98290a4d2f344a6
Marlo Kruse <Plays.xenon@yahoo.de>
2020-05-19 23:07:48 +0200

    Added Aeon Fatboy

--- #Marlos mods compilation/mod_info.lua
+++ #Marlos mods compilation/mod_info.lua
@@ -1,11 +1,11 @@
 name = "Marlo's mod Compilation"
-version = 102
+version = 103
 copyright = "Copyright © 2018, Electronic Farts co"
 description = "This mod contains multiple new units (other factions are WIP) Disclaimer: These Units were made/edited by me MarLo aka Degulum, based on vanilla units with varying quality and uniquemess"
 author = "Degulum"
 icon = "/mods/#Marlos mods compilation/icon.bmp"
 url = ""
-uid = "e3hueff354-d1EE1-2gf2-14r8-2467d3c1s38cd"
+uid = "e3hueff354-d1FF4-2gf2-14r8-2467d3c1s38cd"
 
 exclusive = false
 
