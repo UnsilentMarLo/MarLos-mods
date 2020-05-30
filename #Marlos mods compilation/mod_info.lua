e72308351ebd028db1f4e5ee24355f42478bb14a
Marlo Kruse <Plays.xenon@yahoo.de>
2020-05-30 22:43:03 +0200

    added Anti aliasing to the HMP

--- #Marlos mods compilation/mod_info.lua
+++ #Marlos mods compilation/mod_info.lua
@@ -1,11 +1,11 @@
 name = "Marlo's mod Compilation"
-version = 109
+version = 110
 copyright = "Copyright © 2018, Electronic Farts co"
 description = "This mod contains multiple new units (other factions are WIP) Disclaimer: These Units were made/edited by me MarLo aka Degulum, based on vanilla units with varying quality and uniquemess"
 author = "Degulum"
 icon = "/mods/#Marlos mods compilation/icon.bmp"
 url = ""
-uid = "e3hueff354-SF521-2gf2-14r8-2467d3c1s38cd"
+uid = "e3hueff524-SF521-2gf2-14r8-2467d3c1s38cd"
 
 exclusive = false
 
